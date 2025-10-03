42c80d8d5b6b058b904c649bfb49fe0d8ca3161c
Kenneth Nicholls <kenneth.nicholls@icloud.com>
2025-10-03 19:48:21 -0400

    class 04

---
+++ 04/lecture.py
@@ -0,0 +1,13 @@
+#Friday October 3. 2025
+#CodeUp 2025 Fall Class
+#Class 04
+# Instructors: George Song, Kevin Yang
+
+#age = int(input("What is yor age: "))
+#if age < 18:
+#    print("You are a Minor")
+#elif age < 60:
+#    print("You are a Adult ")
+#else:
+#    print("You are a Senior ")
+
