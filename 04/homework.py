b0a1fdc233fc5276d4271211c7c5e91ea27f4e6b
Kenneth Nicholls <kenneth.nicholls@icloud.com>
2025-10-03 21:12:08 -0400

    class04

---
+++ 04/homework.py
@@ -0,0 +1,11 @@
+
+# Anderson wants to guess the number of dollars that Oscar
+# has in the shortest amount of tries possible.
+# Oscar has between 1 and 100 dollars, inclusive.
+# Oscar has a random number of dollars, d, and he can tell
+# Anderson if he has more or less dollars than the number Anderson guessed.
+# Your code should randomly select the number of dollars that Oscar has.
+# You will be  acting as Anderson and guessing the number of dollars
+# inside the terminal.
+
+# Bonus: can you find a strategy to find the number using at most 7 tries every time?
