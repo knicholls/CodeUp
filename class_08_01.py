2f71e4e03f9fd8837b4bd9bb81c9f9d7f6c69e44
Kenneth Nicholls <kenneth.nicholls@icloud.com>
2025-11-14 08:30:28 -0500

    class08 am2

--- class_08_01.py
+++ class_08_01.py
@@ -5,6 +5,8 @@ import random
 
 pygame.init()
 screen = pygame.display.set_mode((800, 800))
+pygame.display.set_caption("Game Window")
+
 font = pygame.font.Font( 'freesansbold.ttf', 50)
 small_font = pygame.font.Font( 'freesansbold.ttf', 20)
 text_x = font.render("X", True, (255,255,255))
@@ -26,7 +28,14 @@ while True:
 
     white = ((255,255,255))
     pygame.draw.line(screen, white, (200 + 133, 200),(200 +133, 600))    
-    pygame.draw.line(screen, white, (200 + 133, 200),(200 +133, 600))   
-    pygame.draw.line(screen, white, (200 + 133, 200),(200 +133, 600))   
-    pygame.draw.line(screen, white, (200 + 133, 200),(200 +133, 600))   
-    
+    pygame.draw.line(screen, white, (200 + 133 + 134, 200),(200 + 133 +134, 600))   
+    pygame.draw.line(screen, white, (200, 200 + 133),(600, 200 + 133))   
+    pygame.draw.line(screen, white, (200, 200 + 133 + 134),(600, 200 + 133 + 134))   
+
+    clear_board_button = pygame.draw.rect(screen, (255,0,0),clear_button_rect)
+    text_reset = font.render("Reset", True, (255,255,255))
+    text_reset_rect = text_reset.get_rect()
+    text_reset_rect.center = (400, 90)
+    screen.blit(text_reset, text_reset_rect)
+
+    pygame.display.update()
